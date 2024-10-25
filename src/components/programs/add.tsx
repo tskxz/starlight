ce9c081910babc71a1ae0b61cc6ff10645c1831a
twu148 <twu148@ucr.edu>
2024-10-24 23:35:57 -0700

    Added Add project modal

--- src/components/programs/add.tsx
+++ src/components/programs/add.tsx
@@ -2,25 +2,70 @@ import {
   AlertDialogAction,
   AlertDialogCancel,
   AlertDialogContent,
-  AlertDialogDescription,
+  // AlertDialogDescription,
   AlertDialogFooter,
   AlertDialogHeader,
   AlertDialogTitle,
 } from "@/components/ui/alert-dialog";
 
+import TextInput from "@/components/global/inputs/text";
+import Select from "@/components/global/inputs/select";
+import Textarea from "@/components/global/inputs/textarea";
+
 const Add = () => {
   return (
-    <AlertDialogContent>
+    <AlertDialogContent className="max-w-4xl">
       <AlertDialogHeader>
-        <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
-        <AlertDialogDescription>
-          This action cannot be undone. This will permanently delete your
-          account and remove your data from our servers.
-        </AlertDialogDescription>
+        <AlertDialogTitle className="text-xl font-medium">
+          Add Project
+        </AlertDialogTitle>
+        <TextInput
+          meta={{
+            title: "Title",
+            type: "text",
+            placeholder: "i.e WINC",
+            maxLength: 32,
+            value: "",
+            disabled: false,
+          }}
+        />
+        <Textarea
+          meta={{
+            title: "Description",
+            type: "textarea",
+            placeholder: "Description of your project",
+            maxLength: 500,
+            value: "",
+            disabled: false,
+          }}
+        />
+        <Select
+          meta={{
+            title: "Tech Stack",
+            type: "select",
+            options: ["TypeScript", "Next.js", "TailwindCSS"],
+            value: "",
+            disabled: false,
+          }}
+        />
+        <TextInput
+          meta={{
+            title: "Total Spots",
+            type: "text",
+            placeholder: "e.g 10",
+            maxLength: 5,
+            value: "",
+            disabled: false,
+          }}
+        />
       </AlertDialogHeader>
       <AlertDialogFooter>
-        <AlertDialogCancel>Cancel</AlertDialogCancel>
-        <AlertDialogAction>Add</AlertDialogAction>
+        <AlertDialogCancel className="w-1/6 border-starlight-blue font-bold">
+          Cancel
+        </AlertDialogCancel>
+        <AlertDialogAction className="w-1/6 bg-starlight-blue">
+          Add
+        </AlertDialogAction>
       </AlertDialogFooter>
     </AlertDialogContent>
   );
