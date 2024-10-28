effd230de9b0719b6af2cd3303f5680252ede017
Sarbesh1234 <sankarsarbesh@gmail.com>
2024-10-28 03:58:04 -0700

    Fix formatting issues with Prettier

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
+import Project from "@/components/programs/project";
+
 const Home = () => {
   return (
     <div className="flex h-screen w-screen items-center justify-center">
-      Hello World
+      <Project
+        title="WINC Website"
+        description="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the indsutry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book"
+        tags={["TypeScript", "TypeScript", "TypeScript"]}
+      />
     </div>
   );
 };
