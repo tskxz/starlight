cd26477a6754eb1ee37e81e0df5e210f9488d41e
shahdivyank <divyank.shah.2016@gmail.com>
2024-10-28 16:54:07 -0700

    fix

--- src/components/global/navigation/index.tsx
+++ src/components/global/navigation/index.tsx
@@ -1,14 +1,5 @@
-// import Link from "next/link";
-import SideNav from "../navigation/side";
-
 const Navigation = () => {
-  return (
-    <div className="flex bg-starlight-blue">
-      <SideNav />
-      {/* <Link href="/user/applications">Applications</Link>
-        <Link href="/user/profile">Profile</Link> */}
-    </div>
-  );
+  return <div className="flex bg-starlight-blue"></div>;
 };
 
 export default Navigation;
--- src/components/global/navigation/side.tsx
+++ src/components/global/navigation/side.tsx
@@ -1,16 +1,33 @@
+import Image from "next/image";
 import Link from "next/link";
+import Logo from "@/public/logo.png";
 
-const SideNav = () => {
+interface link {
+  name: string;
+  link: string;
+}
+
+interface props {
+  links: link[];
+}
+
+const SideNav = ({ links }: props) => {
   return (
-    <div className="fixed left-0 top-0 flex h-full w-[10%] flex-col items-center bg-starlight-blue text-white">
-      {/* logo image */}
-      <div className="mb-2 w-full bg-yellow-200 px-4 py-2 text-center text-black">
-        Admin
+    <nav className="flex h-screen w-[10%] flex-col items-center bg-starlight-blue text-white">
+      <Image src={Logo} alt="Starlight Logo" />
+
+      <div className="w-full text-center">
+        {links.map(({ link, name }, index) => (
+          <Link
+            key={index}
+            href={link}
+            className="hover:bg-starlight-yellow !w-full"
+          >
+            {name}
+          </Link>
+        ))}
       </div>
-      <nav className="flex flex-col gap-4">
-        <Link href="/user/applications">Applications</Link>
-      </nav>
-    </div>
+    </nav>
   );
 };
 
