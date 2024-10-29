4df56b67224a856c7f5a6273a6e4253c87adf5c6
shahdivyank <divyank.shah.2016@gmail.com>
2024-10-28 17:01:25 -0700

    fix button

--- src/components/global/button.tsx
+++ src/components/global/button.tsx
@@ -1,9 +1,17 @@
-interface props {
-  children: React.ReactNode;
+interface ButtonProps {
+  children: string;
+  onClick: () => void;
 }
 
-const Button = ({ children }: props) => {
-  return <div className="bg-starlight-blue text-white">{children}</div>;
+const Button = ({ children, onClick }: ButtonProps) => {
+  return (
+    <button
+      className="rounded-md bg-starlight-blue px-8 py-2 font-serif text-white hover:opacity-80"
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  );
 };
 
 export default Button;
