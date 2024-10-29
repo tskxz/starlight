93db90aad19cebafd2dc99e9448e0e9a1a8c02bb
shahdivyank <divyank.shah.2016@gmail.com>
2024-10-28 18:56:30 -0700

    nice

--- src/components/programs/add.tsx
+++ src/components/programs/add.tsx
@@ -1,70 +1,69 @@
+"use client";
 import {
   AlertDialogAction,
   AlertDialogCancel,
   AlertDialogContent,
-  // AlertDialogDescription,
   AlertDialogFooter,
   AlertDialogHeader,
-  AlertDialogTitle,
 } from "@/components/ui/alert-dialog";
 
-import TextInput from "@/components/global/inputs/text";
+import Input from "@/components/global/inputs/input";
 import Select from "@/components/global/inputs/select";
 import Textarea from "@/components/global/inputs/textarea";
+import { Label } from "../ui/label";
+import Button from "../global/button";
 
 const Add = () => {
   return (
     <AlertDialogContent className="max-w-4xl">
       <AlertDialogHeader>
-        <AlertDialogTitle className="text-xl font-medium">
-          Add Project
-        </AlertDialogTitle>
-        <TextInput
-          meta={{
-            title: "Title",
-            type: "text",
-            placeholder: "i.e WINC",
-            maxLength: 32,
-            value: "",
-            disabled: false,
-          }}
-        />
-        <Textarea
-          meta={{
-            title: "Description",
-            type: "textarea",
-            placeholder: "Description of your project",
-            maxLength: 500,
-            value: "",
-            disabled: false,
-          }}
-        />
-        <Select
-          meta={{
-            title: "Tech Stack",
-            type: "select",
-            options: ["TypeScript", "Next.js", "TailwindCSS"],
-            value: "",
-            disabled: false,
-          }}
-        />
-        <TextInput
-          meta={{
-            title: "Total Spots",
-            type: "text",
-            placeholder: "e.g 10",
-            maxLength: 5,
-            value: "",
-            disabled: false,
-          }}
-        />
+        <Label className="text-xl">Add New Project</Label>
       </AlertDialogHeader>
+      <Input
+        meta={{
+          title: "Title",
+          type: "text",
+          placeholder: "i.e WINC",
+          maxLength: 32,
+          value: "",
+          disabled: false,
+        }}
+      />
+      <Textarea
+        meta={{
+          title: "Description",
+          type: "textarea",
+          placeholder: "Description of your project",
+          maxLength: 500,
+          value: "",
+          disabled: false,
+        }}
+      />
+      <Select
+        meta={{
+          title: "Tech Stack",
+          type: "select",
+          options: ["TypeScript", "Next.js", "TailwindCSS"],
+          value: "",
+          disabled: false,
+        }}
+      />
+      <Input
+        meta={{
+          title: "Total Spots",
+          type: "text",
+          placeholder: "e.g 10",
+          maxLength: 5,
+          value: "",
+          disabled: false,
+        }}
+      />
       <AlertDialogFooter>
-        <AlertDialogCancel className="w-1/6 border-starlight-blue font-bold">
-          Cancel
+        <AlertDialogCancel asChild>
+          <Button onClick={() => {}}>Cancel</Button>
         </AlertDialogCancel>
-        <AlertDialogAction className="w-1/6 bg-starlight-blue">
-          Add
+        <AlertDialogAction asChild>
+          <Button onClick={() => {}}>Add</Button>
         </AlertDialogAction>
       </AlertDialogFooter>
     </AlertDialogContent>
